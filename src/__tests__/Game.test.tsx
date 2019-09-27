09fd36b85a8b7f24be2a2502c35218c750bec6e8
Madan Lalprajapati <madan.l@HCL.COM>
2019-09-27 14:40:47 +0530

    added test for index

--- src/__tests__/Game.test.tsx
+++ src/__tests__/Game.test.tsx
@@ -29,3 +29,49 @@ it('renders game status correctly', () => {
     const winner = wrapper.find('div.game-info').children().first().text()
     expect(winner).toEqual('Winner: X')
   })
+  it('renders game No Winner correctly', () => {
+    const wrapper = mount(<Game/>)
+    const firstPlayer = wrapper.find('div.game-info').children().first().text()
+    expect(firstPlayer).toEqual('Next player: X')
+  const button = wrapper.find('button.square').first()
+    button.simulate('click')
+    const secondPlayer = wrapper.find('div.game-info').children().first().text()
+    expect(secondPlayer).toEqual('Next player: O')
+  //player 2
+    const turn1 = wrapper.find('button.square').at(1)
+    turn1.simulate('click')
+    //player 1
+    const turn2 = wrapper.find('button.square').at(1)
+    turn2.simulate('click')
+        
+    const winner = wrapper.find('div.game-info').children().first().text()
+    expect(winner).toEqual('Next player: X')
+  })
+  
+  it('renders game jump Button action correctly', () => {
+    const wrapper = mount(<Game/>)
+    const firstPlayer = wrapper.find('div.game-info').children().first().text()
+    expect(firstPlayer).toEqual('Next player: X')
+  const button = wrapper.find('button.square').first()
+    button.simulate('click')
+    const secondPlayer = wrapper.find('div.game-info').children().first().text()
+    expect(secondPlayer).toEqual('Next player: O')
+  //player 2
+    const turn2 = wrapper.find('button.square').at(1)
+    turn2.simulate('click')
+    //player 1
+    const turn3 = wrapper.find('button.square').at(4)
+    turn3.simulate('click')
+    //player 2
+    const turn4 = wrapper.find('button.square').at(5)
+    turn4.simulate('click')
+    //player 1
+    const turn5 = wrapper.find('button.square').at(8)
+    turn5.simulate('click')
+    
+    const moveButton = wrapper.find('div.game-info').children().find('button').first();
+    moveButton.simulate('click');
+    const s1 = wrapper.find('button.square').at(0).text()
+
+    expect(s1).toBe('');
+  })
